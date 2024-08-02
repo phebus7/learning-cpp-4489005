437af5eec458ee592fcda1c91970accc017862fe
phebus7 <phebus7@hotmail.com>
2024-08-02 15:47:43 -0400

    restore

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -5,9 +5,9 @@
 #include <iostream>
 
 int main(){
-    float num_1, num_2, result; //
+    float num_1, num_2, result;
 
-    std::cout << "Enter number 1: " << std::flush; //
+    std::cout << "Enter number 1: " << std::flush;
     std::cin >> num_1;
     std::cout << "Enter number 2: " << std::flush;
     std::cin >> num_2;
